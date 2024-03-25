76d12a4382c69fbd69219d264a9c772c25fc4ba9
Ole Holm Frandsen <froksen@gmail.com>
2024-03-25 21:03:06 +0100

    Update updateandrun.bat
    Ændret således der tjekkes for VENV-miljø fremfor at genoprette heletiden

--- updateandrun.bat
+++ updateandrun.bat
@@ -10,8 +10,13 @@ echo ************************************
 
 echo "TRIN 1: Henter nyeste version"
 git pull
-echo "TRIN 2: Opretter VENV-miljø"
-python -m venv venv
+echo "TRIN 2: Undersøger om VENV-miljø findes"
+if exist venv\ (
+  echo "- VENV-miljø blev fundet. Fortsætter"
+) else (
+  echo "- VENV-miljø blev ikke fundet. Opretter VENV-miljø"
+  python -m venv venv
+)
 echo "TRIN 3 - Aktiverer VENV"
 call venv\Scripts\activate.bat
 echo "TRIN 4: Installerer nødvendige afhængigheder
