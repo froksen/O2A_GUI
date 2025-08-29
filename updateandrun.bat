d600d0a05b397151138814b58b806e70cd20fab1
Ole Holm Frandsen <froksen@gmail.com>
2025-08-29 20:33:48 +0200

    Sikkerhedskopi af gammelt opdateringsscript

--- updateandrun.bat
+++ updateandrun.bat
@@ -8,23 +8,15 @@ echo ************************************
 echo *          O2A OPDATERING          *
 echo ************************************
 
-echo "TRIN 1: Henter nyeste version"
-git fetch --all
-git reset --hard origin/master
-echo "TRIN 2: Undersøger om VENV-miljø findes"
-if exist venv\ (
-  echo "- VENV-miljø blev fundet. Fortsætter"
-) else (
-  echo "- VENV-miljø blev ikke fundet. Opretter VENV-miljø"
-  py -m venv venv
-)
+echo "TRIN 1: Henter nyeste version fra Github"
+
 
 :: Step 3: Install dependencies
-echo "TRIN 3 Installerer nødvendige afhængigheder.....""
+echo "TRIN 2 Opgraderer PIP og installerer UV.....""
 py -m pip install --upgrade pip
-py -m pip install -r requirements.txt
+py -m pip install uv
 
 cd /D "%~dp0"
-echo "TRIN 4 Afvikler programmet.....""
-start main.pyw
+echo "TRIN 3 Afvikler programmet.....""
+start uv run main.pyw
 exit
